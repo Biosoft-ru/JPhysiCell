8701d935e26ad82929190cb466ca94ccc112f0db
Ilya <damagedone@yandex.ru>
2025-07-30 13:16:46 +0400

    ID removed from report

--- src/main/java/ru/biosoft/physicell/core/ReportGenerator.java
+++ src/main/java/ru/biosoft/physicell/core/ReportGenerator.java
@@ -4,12 +4,12 @@ public class ReportGenerator
 {
     public String[] getReportHeaderElements()
     {
-        return new String[] {"ID", "X", "Y", "Z", "Cycle", "Elapsed"};
+        return new String[] {"X", "Y", "Z", "Cycle", "Elapsed"};
     }
 
     public Object[] getReportElements(Cell cell) throws Exception
     {
-        return new Object[] {cell.ID, cell.position[0], cell.position[1], cell.position[2], cell.phenotype.cycle.currentPhase().name,
+        return new Object[] {cell.position[0], cell.position[1], cell.position[2], cell.phenotype.cycle.currentPhase().name,
                 cell.phenotype.cycle.data.elapsedTimePhase};
     }
 }
